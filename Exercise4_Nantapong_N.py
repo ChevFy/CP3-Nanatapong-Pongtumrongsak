96953155f3c9cff4bdb034b65c953b5233b330bb
ChevFy <135439190+ChevFy@users.noreply.github.com>
2023-06-08 17:28:30 +0700

    Create Exercise4_Nantapong_N.py

---
+++ Exercise4_Nantapong_N.py
@@ -0,0 +1,12 @@
+
+F_ENG = 4.0
+G_BN = 9.0
+IN_COMSYS = 10.0
+COM_PG = 10.0
+
+print("---------Your score---------")
+print("Foundation Englinsh = ",F_ENG)
+print("General Buisness = ",G_BN)
+print("Introdruction to Computer System = ",IN_COMSYS)
+print("Foundation Englinsh = ",COM_PG)
+print("----------------------------")
