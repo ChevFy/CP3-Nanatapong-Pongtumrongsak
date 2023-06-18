561a1bede4e47306299b5a1e36d896b766c691fd
ChevFy <135439190+ChevFy@users.noreply.github.com>
2023-06-18 20:10:24 +0700

    Create lecture54.py

---
+++ lecture54.py
@@ -0,0 +1,66 @@
+'''
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+print(priceCalculator())
+
+'''
+
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        print("-----Wrong-----")
+        return login()
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    return menuSelect()
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1 :
+        return vatCalculator()
+    elif userSelected ==2 :
+        return  priceCalculator()
+
+def vatCalculator():
+    totalPrice = int(input("Price :"))
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return (price1 + price2)
+
+print(login())
